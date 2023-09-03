b1e971bc9d68435bf3ad79a385e40596cc3f4231
Ugo Asonye <ugonnaasonye@yahoo.com>
2023-09-03 11:12:44 -0500

    Add in create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,14 @@ class UsersController < ApplicationController
     @users = User.all
     render :index
   end
+
+  def create
+    @user = User.create(
+      username: params[:username],
+      email: params[:email],
+      password: params[:password],
+      profile_pic: params[:profile_pic],
+    )
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get "/users" => "users#index"
+  post "/users" => "users#create"
 end
