f3c9f2b6b4782576f632f55d5a76d59d33131a80
Ugo Asonye <ugonnaasonye@yahoo.com>
2023-09-03 11:47:44 -0500

    Add update method for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,15 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     render :show
   end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.update(
+      username: params[:username] || @user.username,
+      email: params[:email] || @user.email,
+      password: params[:password] || @user.password,
+      profile_pic: params[:profile_pic] || @user.profile_pic,
+    )
+    render :show
+  end
 end
