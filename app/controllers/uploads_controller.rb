07f311409c0eb8fb18c6bdacda7abecb57fd4be2
Ugo Asonye <ugonnaasonye@yahoo.com>
2023-09-03 14:03:42 -0500

    Add show  for uploads

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -7,10 +7,15 @@ class UploadsController < ApplicationController
   def create
     @upload = Upload.create(
       title: params[:title],
+      description: params[:description],
+      image: params[:image],
+      user_id: params[:user_id],
     )
     render :show
   end
 
   def show
+    @upload = Upload.find_by(id: params[:id])
+    render :show
   end
 end
